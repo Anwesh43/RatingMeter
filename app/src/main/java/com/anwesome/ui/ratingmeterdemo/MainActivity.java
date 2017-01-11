16a3e816618e5aa504f17e31a50281660dd1bcbf
Anwesh43 <anweshthecool0@gmail.com>
2017-01-12 03:40:53 +0530

    testing return of ratingview

--- app/src/main/java/com/anwesome/ui/ratingmeterdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/ratingmeterdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.anwesome.ui.ratingmeter.RatingMeter;
+import com.anwesome.ui.ratingmeter.RatingView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RatingMeter ratingMeter = new RatingMeter(this);
-        ratingMeter.addRatings(200,200);
+        RatingView ratingView = ratingMeter.addRatings(200,200);
     }
 }
